73ab8f23127b8e740f527b705699a4f545c54db3
farSIght <danivantyshyn@gmail.com>
2019-08-01 00:11:06 +0300

    views setup

--- app/src/main/java/com/example/testapp/view/MainActivity.kt
+++ app/src/main/java/com/example/testapp/view/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.testapp.view
 
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import com.example.testapp.R
 import com.example.testapp.utils.extensions.nonNullObserve
 import com.example.testapp.viewModel.main.MainViewModel
@@ -8,6 +10,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : BaseActivity() {
+    private val items = (1..20).map { "item $it" }
 
     private val vm: MainViewModel by viewModel()
 
@@ -15,8 +18,13 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        vm.fullName.nonNullObserve(this) { fullname.text = it }
-        vm.email.nonNullObserve(this) { email.text = it }
+        recyclerView.apply {
+            adapter = SimpleAdapter(items)
+            layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+        }
+
+//        vm.fullName.nonNullObserve(this) { fullname.text = it }
+//        vm.email.nonNullObserve(this) { email.text = it }
         vm.load()
     }
 }
